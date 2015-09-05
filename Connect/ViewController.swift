fa0e1a25c479170e12454f1e0b4b3c0bf2911b4d
wilzh40 <wilzh40@gmail.com>
2015-09-05 11:26:17 -0400

    Wrote an extension for string

--- Connect/ViewController.swift
+++ Connect/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
+extension String {
+    func ascii2uint() -> UInt8 {
+        let x = self.utf8
+        return x[x.startIndex]
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let demo = AudioDemo()
+     
+        demo.generator.writeByte("W".ascii2uint())
+        demo.generator.writeByte("I".ascii2uint())
+        demo.generator.writeByte("L".ascii2uint())
+        demo.generator.writeByte("S".ascii2uint())
+        demo.generator.writeByte("O".ascii2uint())
+        demo.generator.writeByte("N".ascii2uint())
+        demo.generator.writeByte(0xFF)
+     
         demo.generator.writeByte(0xFF)
         demo.generator.writeByte(0xFF)
         demo.generator.writeByte(0xFA)
@@ -20,8 +36,7 @@ class ViewController: UIViewController {
         demo.generator.writeByte(0xFF)
         demo.generator.writeByte(0xFF)
         demo.generator.writeByte(0xFF)
-     
-
+        
       /*
         let config = JMFSKModemConfiguration.highSpeedConfiguration()
         let modem = JMFSKModem.init(configuration: config)
@@ -44,6 +59,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+   
+    
+   /* func hex2ascii (example: String) -> String
+    {
+        
+        var chars = [Character]()
+        
+        for c in example.characters
+        {
+            chars.append(c)
+        }
+        
+        let numbers =  stride(from: 0, to: chars.count, by: 2).map{
+            strtoul(String(chars[$0 ..< $0+2]), nil, 16)
+        }
+        
+        var final = ""
+        var i = 0
+        
+        while i < numbers.count {
+            final.append(Character(UnicodeScalar(Int(numbers[i]))))
+            i++
+        }
+        
+        return final
+    }*/
 
 }
 
